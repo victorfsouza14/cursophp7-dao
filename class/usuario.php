27d27c031f8d7df7854db4c2f458099426bccb39
Victor <victorfsouza14@hotmail.com>
2019-10-15 10:43:03 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -65,6 +65,7 @@
 
 			));
 
+
 			if(count($results) > 0) {
 
 				$row = $results[0];
@@ -76,6 +77,57 @@
 
 			}
 		}
+
+		public static function getList(){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+		}
+
+		public static function search($login){
+
+
+			$sql = new Sql();
+			return $sql->Select("SELECT * FROM tb_usuarios  WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+				':SEARCH' => "%".$login."%"
+
+			));
+
+		}
+
+		public function login($login, $password){
+
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password
+
+			));
+
+
+			if(count($results) > 0) {
+
+				$row = $results[0];
+
+				$this-> setIdusuario($row['idusuario']);
+				$this-> setDeslogin($row['deslogin']);
+				$this-> setDessenha($row['dessenha']);
+				$this-> setDtcadastro( new DateTime($row['dtcadastro']));
+
+			} else {
+
+				throw new Exception("Login e/ou senha invalidos.");
+				
+
+			}
+
+		}	
+
 
 		public function __toString(){
 
--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuário//
+
+/*$root = new Usuario();
 
 	$root->loadById(3);
 
 	echo $root;
+*/
+
+
+//carrega um usuário com pula linha
 
 /*$sql = new Sql();
 
@@ -18,5 +24,36 @@ foreach ($usuarios as $usuario) {
 	echo "<br>";
 }
 */
+
+
+//carrega uma lista de usuario//
+
+/*$lista = usuario::getList();
+
+foreach ($lista as $list){
+
+echo json_encode($list);
+echo "<br>";
+}
+*/
+
+//carrega um usuario de procura//
+
+/*$search = usuario::search("jo");
+
+foreach ($search as $procura){
+
+	echo json_encode($procura);
+	echo "<br>";
+
+}
+*/
+
+//carrega um login validado//
+
+$loginValidado = new usuario();
+$loginValidado->login("joao", "qwerty");
+
+echo $loginValidado;
 
 ?>
